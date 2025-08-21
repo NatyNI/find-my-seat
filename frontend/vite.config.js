d5f9f23ef14bcf5bd19c3c0b398f3c1307ecb17a
NatyNi <natynicolae2002@gmail.com>
2025-08-21 11:48:21 +0300

    Allow domain in React server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['findmyseat.website', 'localhost', '127.0.0.1'],
+  },
   plugins: [react()],
 })
