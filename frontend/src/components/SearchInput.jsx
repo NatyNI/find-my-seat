e32aa9da41232c9f154dbc98616cf9cfe02d28f9
NatyNi <natynicolae2002@gmail.com>
2025-09-14 17:16:44 +0300

    Path to server

--- frontend/src/components/SearchInput.jsx
+++ frontend/src/components/SearchInput.jsx
@@ -15,7 +15,7 @@ const SearchInput = () =>{
 
 
     useEffect(() => {
-            axios.get(`http://127.0.0.0.:8000/search`)
+            axios.get(`/api/search`)
                 .then(response =>{
                     setDataDB(response.data)
                 })
