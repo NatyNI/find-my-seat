105eb18d2e2f98ad5e24f377a38f31dc91a85ccb
NatyNi <natynicolae2002@gmail.com>
2025-09-18 08:13:04 +0300

    Add color Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@
 const Header = () =>{
 
     return(
-        <div className=" bg-amber-700 fixed w-full text-white text-center pt-4">
+        <div className=" bg-gradient-to-b from-amber-800/100 via-amber-800/80 to-amber-800/40 fixed w-full text-white text-center pt-4 ">
             <h1 className=" text-lg ">
                 Bine ati venit la nunta lui 
             </h1>
