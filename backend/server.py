9019db0ce1fcb3437780b4a5aae9bb44b403c040
NatyNi <natynicolae2002@gmail.com>
2025-10-01 20:11:08 +0300

    Rezolve the problem with images serve

--- backend/server.py
+++ backend/server.py
@@ -14,14 +14,5 @@ def sendNames():
     
     return jsonify(data)
 
-# @app.route("/images", methods=["GET"])
-# def sendImage():
-#     nrTable = request.args.get('nrTable')
-#     app.logger.info(f"Primit nrTable = {nrTable}")
-#     print(nrTable)
-#     imageUrl = f'static/images/{nrTable}.png'
-#     print(imageUrl)
-#     return send_file(imageUrl)
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=False)
--- frontend/src/components/SearchInput.jsx
+++ frontend/src/components/SearchInput.jsx
@@ -158,3 +158,4 @@ const SearchInput = () =>{
 };
 
 export default SearchInput;
+
