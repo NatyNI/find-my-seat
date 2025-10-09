ad1d8aef4438b4e7b4292cc51f7052ccc27dcfbb
NatyNi <natynicolae2002@gmail.com>
2025-10-09 15:39:58 +0300

    Add style on ShowTable component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () =>{
 
     return(
-        <div className=" fixed w-full bottom-0  p-4  text-center text-black">
+        <div className=" fixed w-full bottom-0 p-4 text-center text-black animate-pulse">
             <h2 className=" inset-0 -z-10 rounded-full bg-[radial-gradient(ellipse_at_center,_rgba(22,163,74,0.8)_0%,_rgba(22,163,74,0)_80%)] ">
                 Apele cele mari nu pot să stingă dragostea și râurile n-ar putea s-o înece.
             </h2>
