4b886810207f33c8e67a5e240284be51a46ddfaa
NatyNi <natynicolae2002@gmail.com>
2025-10-09 21:16:08 +0300

    Add style on ShowTable component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () =>{
     return(
         <div className=" fixed w-full bottom-0 p-4 text-center text-black animate-pulse">
             <h2 className=" inset-0 -z-10 rounded-full bg-[radial-gradient(ellipse_at_center,_rgba(22,163,74,0.8)_0%,_rgba(22,163,74,0)_80%)] ">
-                Mulțumim pentru prezența dumneavoastră try NOW!
+                Mulțumim pentru prezența dumneavoastră!
             </h2>
             <h3 className="italic inset-0 -z-10 rounded-full bg-[radial-gradient(ellipse_at_center,_rgba(22,163,74,0.8)_0%,_rgba(22,163,74,0)_80%)]">
                 Cu drag, Andrei & Miriam!
