fb1b821354fe8e2b6753253a13472e40adf26835
NatyNi <natynicolae2002@gmail.com>
2025-10-09 17:50:36 +0300

    Add style on ShowTable component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,10 +4,10 @@ const Footer = () =>{
     return(
         <div className=" fixed w-full bottom-0 p-4 text-center text-black animate-pulse">
             <h2 className=" inset-0 -z-10 rounded-full bg-[radial-gradient(ellipse_at_center,_rgba(22,163,74,0.8)_0%,_rgba(22,163,74,0)_80%)] ">
-                Apele cele mari nu pot să stingă dragostea și râurile n-ar putea s-o înece.
+                Mulțumim pentru prezența dumneavoastră!
             </h2>
             <h3 className="italic inset-0 -z-10 rounded-full bg-[radial-gradient(ellipse_at_center,_rgba(22,163,74,0.8)_0%,_rgba(22,163,74,0)_80%)]">
-                📖 Cântarea Cântărilor 8:7a
+                Cu drag, Andrei & Miriam!
             </h3>
         </div>
     )
