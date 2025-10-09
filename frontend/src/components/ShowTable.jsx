8774fad10f567324b0786cfb292fc875e796937e
NatyNi <natynicolae2002@gmail.com>
2025-10-09 12:07:06 +0300

    Add the plan of restaurant

--- frontend/src/components/ShowTable.jsx
+++ frontend/src/components/ShowTable.jsx
@@ -24,7 +24,7 @@ const ShowTable = () =>{
             <h2 className="pt-2">
                 {kids}
             </h2> 
-            <img className="w-full h-full"
+            <img className="w-full h-[420px]"
                 src={"https://findmyseat.website/static/images/last.png"} alt="Seat"
             /> <br/>
             <button
