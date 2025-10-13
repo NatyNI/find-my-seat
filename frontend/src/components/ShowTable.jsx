1764f703860cb8c2ba2d26fdae721b8da77ecb21
NatyNi <natynicolae2002@gmail.com>
2025-10-13 12:31:00 +0300

    Add style on main page

--- frontend/src/components/ShowTable.jsx
+++ frontend/src/components/ShowTable.jsx
@@ -11,7 +11,7 @@ const ShowTable = () =>{
 
     return(
 
-        <div className="flex flex-col items-center p-1 overflow-hidden">
+        <div className="flex flex-col items-center p-1 ">
             <h1 className="mt-5 border-t-2 border-b-2 rounded border-gray-500 text-lg font-bold animate-bounce">
                 {name}
             </h1>
