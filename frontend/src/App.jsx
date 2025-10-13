50d7e8a870acca4930dcf64908b7468efd786da4
NatyNi <natynicolae2002@gmail.com>
2025-10-13 11:39:17 +0300

    Eliminate scroll on page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Route
           path="/"
           element={
-            <div className="flex flex-col min-h-screen">
+            <div className="flex flex-col h-screen overflow-hidden">
 
               <header>
                 <Header/>
